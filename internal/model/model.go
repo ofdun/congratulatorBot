2267a257b22bf0172403bf2c3df6ca6961d7bdef
ofdun <elik394@mail.ru>
2023-10-29 01:48:18 +0300

    Initializators added

--- internal/model/model.go
+++ internal/model/model.go
@@ -5,7 +5,21 @@ type Holiday struct {
 	Href string
 }
 
+func NewHoliday(name string, href string) *Holiday {
+	return &Holiday{
+		Name: name,
+		Href: href,
+	}
+}
+
 type Postcard struct {
 	Holiday string
 	Href    string
+}
+
+func NewPostcard(holiday string, href string) *Postcard {
+	return &Postcard{
+		Holiday: holiday,
+		Href:    href,
+	}
 }
