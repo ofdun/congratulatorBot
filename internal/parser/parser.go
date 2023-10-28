869155e10d7695964e9451ab68e9ad6006b01c13
ofdun <elik394@mail.ru>
2023-10-29 01:50:25 +0300

    Parsing the site and getting Holidays from it

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,13 +1,21 @@
 package parser
 
 import (
+	"github.com/PuerkitoBio/goquery"
 	"io"
 	"net/http"
+	"strings"
+	"telegramBot/internal/model"
+	"unicode"
 )
 
 type Parser struct {
 	URL  string
 	HTML string
+}
+
+func NewParser(url string) *Parser {
+	return &Parser{URL: url}
 }
 
 func (p *Parser) GetHTML() error {
@@ -28,4 +36,45 @@ func (p *Parser) GetHTML() error {
 		return err
 	}
 	return nil
+}
+
+func ParseDateFromString(date string) string {
+	newDate := ""
+	for _, char := range date {
+		if unicode.IsLetter(char) || unicode.IsDigit(char) {
+			newDate += string(char)
+		} else if char == ' ' {
+			break
+		}
+	}
+	return newDate
+}
+
+func (p *Parser) GetHolidays() ([]model.Holiday, error) {
+	var holidays []model.Holiday
+	var holidayTitle, holidayDay string
+
+	document, err := goquery.NewDocumentFromReader(strings.NewReader(p.HTML))
+	if err != nil {
+		return holidays, err
+	}
+
+	document.Find(".album-info").Each(func(i int, s *goquery.Selection) {
+
+		s.Find(".name").Each(func(i int, s *goquery.Selection) {
+			if i == 0 {
+				holidayTitle = s.Text()
+			} else if i == 2 {
+				UnparsedDate := s.Text()
+				holidayDay = ParseDateFromString(UnparsedDate)
+			}
+		})
+		holiday := model.NewHoliday(
+			holidayTitle,
+			holidayDay,
+		)
+		holidays = append(holidays, *holiday)
+	})
+
+	return holidays, err
 }
