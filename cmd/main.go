79c6572483a80d9eab5c6e7d6ed6ec0a787888b2
ofdun <elik394@mail.ru>
2023-10-29 01:49:40 +0300

    Printing Todays Holidays

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,29 @@
-package cmd
+package main
+
+import (
+	"fmt"
+	"telegramBot/internal/parser"
+)
+
+func main() {
+	newParser := parser.NewParser(
+		"https://3d-galleru.ru/archive/cat/kalendar-42/")
+
+	err := newParser.GetHTML()
+	if err != nil {
+		panic(err)
+	}
+
+	holidays, err := newParser.GetHolidays()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, h := range holidays {
+		fmt.Println(h)
+	}
+
+	//html := newParser.HTML
+	//
+	//println(html)
+}
