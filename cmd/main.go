feb76b31948528d996765930c21fd81988e4ccb0
ofdun <elik394@mail.ru>
2023-11-02 00:56:36 +0300

    new test

--- cmd/main.go
+++ cmd/main.go
@@ -1,42 +1,54 @@
 package main
 
 import (
-	"telegramBot/internal/download"
-	"telegramBot/internal/parser"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	newParser := parser.NewParser(
-		"https://3d-galleru.ru/archive/cat/kalendar-42/")
-
-	err := newParser.GetHTML()
-	if err != nil {
-		panic(err)
-	}
-
-	holidays, err := newParser.GetHolidays()
-	if err != nil {
-		panic(err)
-	}
-
-	postcards, err := newParser.GetPostcardsPages(holidays[0])
-
-	if err != nil {
-		panic(err)
-	}
-
-	// Updating postcards ( adding links )
-	for i := range postcards {
-		err = newParser.GetPostcardHref(&postcards[i])
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	for i := range postcards {
-		err = download.PostcardDownload("internal/storage/postcards/", &postcards[i])
-		if err != nil {
-			panic(err)
-		}
-	}
+	//postcardsDatabase := storage.NewDatabase()
+	//postcardsStorage := storage.NewPostcardsPostgresStorage(postcardsDatabase)
+	//
+	//newParser := parser.NewParser(
+	//	"https://3d-galleru.ru/archive/cat/kalendar-42/")
+	//
+	//if err := newParser.GetHTML(); err != nil {
+	//	panic(err)
+	//}
+	//
+	//holidays, err := newParser.GetHolidays()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//postcards, err := newParser.GetPostcardsPages(holidays[0])
+	//
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//// Updating postcards ( adding links )
+	//for i := range postcards {
+	//	if err = newParser.GetPostcardHref(&postcards[i]); err != nil {
+	//		panic(err)
+	//	}
+	//}
+	//
+	//for i := range postcards {
+	//	if err = download.PostcardDownload("internal/storage/postcards/", &postcards[i]); err != nil {
+	//		panic(err)
+	//	}
+	//}
+	//
+	//for i := range postcards {
+	//	if err = postcardsStorage.AddPostcard(&postcards[i]); err != nil {
+	//		panic(err)
+	//	}
+	//}
+	//
+	//postcardsFromStorage, err := postcardsStorage.GetPostcardsFromStorage()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//fmt.Println(postcardsFromStorage)
 }
