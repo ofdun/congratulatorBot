aa52d92b8323025f915fd38aa79d719cccb67f1b
ofdun <elik394@mail.ru>
2023-11-02 20:12:42 +0300

    polling tg

--- cmd/main.go
+++ cmd/main.go
@@ -2,53 +2,68 @@ package main
 
 import (
 	_ "github.com/lib/pq"
+	"telegramBot/internal/bots"
+	"telegramBot/internal/download"
+	"telegramBot/internal/parser"
+	"telegramBot/internal/storage"
 )
 
 func main() {
-	//postcardsDatabase := storage.NewDatabase()
-	//postcardsStorage := storage.NewPostcardsPostgresStorage(postcardsDatabase)
-	//
-	//newParser := parser.NewParser(
-	//	"https://3d-galleru.ru/archive/cat/kalendar-42/")
-	//
-	//if err := newParser.GetHTML(); err != nil {
-	//	panic(err)
-	//}
-	//
-	//holidays, err := newParser.GetHolidays()
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//postcards, err := newParser.GetPostcardsPages(holidays[0])
-	//
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//// Updating postcards ( adding links )
-	//for i := range postcards {
-	//	if err = newParser.GetPostcardHref(&postcards[i]); err != nil {
-	//		panic(err)
-	//	}
-	//}
-	//
-	//for i := range postcards {
-	//	if err = download.PostcardDownload("internal/storage/postcards/", &postcards[i]); err != nil {
-	//		panic(err)
-	//	}
-	//}
-	//
-	//for i := range postcards {
-	//	if err = postcardsStorage.AddPostcard(&postcards[i]); err != nil {
-	//		panic(err)
-	//	}
-	//}
-	//
-	//postcardsFromStorage, err := postcardsStorage.GetPostcardsFromStorage()
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//fmt.Println(postcardsFromStorage)
+	postcardsDatabase := storage.NewDatabase()
+	postcardsStorage := storage.NewPostcardsPostgresStorage(postcardsDatabase)
+	if err := postcardsStorage.ClearDatabase(); err != nil {
+		panic(err)
+	}
+
+	newParser := parser.NewParser(
+		"https://3d-galleru.ru/archive/cat/kalendar-42/")
+
+	if err := newParser.GetHTML(); err != nil {
+		panic(err)
+	}
+
+	holidays, err := newParser.GetHolidays()
+	if err != nil {
+		panic(err)
+	}
+
+	postcards, err := newParser.GetPostcardsPages(holidays[0])
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Updating postcards ( adding links )
+	for i := range postcards {
+		if err = newParser.GetPostcardHref(&postcards[i]); err != nil {
+			panic(err)
+		}
+	}
+
+	for i := range postcards {
+		if err = download.PostcardDownload("internal/storage/postcards/", &postcards[i]); err != nil {
+			panic(err)
+		}
+	}
+
+	for i := range postcards {
+		if err = postcardsStorage.AddPostcardToStorage(&postcards[i]); err != nil {
+			panic(err)
+		}
+	}
+
+	token := "6114787188:AAH32av_TK7_Jk_HFXKOGE1FCcqo9XSNpGs"
+	bot, err := bots.NewTelegramBot(token)
+	if err != nil {
+		panic(err)
+	}
+
+	updates, err := bots.SetupUpdates(&bot)
+	if err != nil {
+		panic(err)
+	}
+
+	go bots.StartBotPolling(&bot, updates)
+
+	select {}
 }
