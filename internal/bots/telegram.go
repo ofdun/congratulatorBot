fae163a2967886ed95517c186917b8b1bf685ad8
ofdun <elik394@mail.ru>
2023-11-04 21:45:25 +0300

    Markups and answering default requests

--- internal/bots/telegram.go
+++ internal/bots/telegram.go
@@ -24,8 +24,34 @@ func NewTelegramBot(token string) (*TelegramBot, error) {
 
 func StartTelegramBot(bot *TelegramBot) {
 	bot.Handle(tele.OnText, onText)
+	bot.Handle("/start", onStart)
 
 	bot.Start()
+}
+
+func createMenu(isMailing bool) *tele.ReplyMarkup {
+	markup := tele.ReplyMarkup{}
+	if isMailing {
+		markup.Reply(markup.Row(
+			markup.Text("Ура!🎉"),
+			markup.Text("Поздравь меня🥳"),
+			markup.Text("Отключить рассылку📬"),
+		))
+		return &markup
+	} else {
+		markup.Reply(markup.Row(
+			markup.Text("Ура!🎉"),
+			markup.Text("Поздравь меня🥳"),
+			markup.Text("Подключить рассылку📬"),
+		))
+		return &markup
+	}
+
+}
+
+func onStart(c tele.Context) error {
+	markup := createMenu(false)
+	return c.Send("Привет! Я поздравлю тебя с праздником!", markup)
 }
 
 func onText(c tele.Context) error {
@@ -33,23 +59,36 @@ func onText(c tele.Context) error {
 	case "Ура!🎉":
 		return c.Send("Ура!🎉")
 	case "Поздравь меня🥳":
-		postcardPath, err := storage.GetRandomPostcardPath()
-		if err != nil {
-			return err
-		}
+		{
+			postcardPath, err := storage.GetRandomPostcardPath()
+			if err != nil {
+				return err
+			}
 
-		if postcardPath == "" {
-			return c.Send("Сегодня нет праздников :(")
-		}
+			markup := createMenu(false)
+			if postcardPath == "" {
+				return c.Send("Сегодня нет праздников :(", markup)
+			}
 
-		file := tele.FromDisk(postcardPath)
-		if download.IsVideo(postcardPath) {
-			postcard := &tele.Video{File: file}
-			return c.Send(postcard)
-		} else {
-			postcard := &tele.Photo{File: file}
-			return c.Send(postcard)
+			file := tele.FromDisk(postcardPath)
+			if download.IsVideo(postcardPath) {
+				postcard := &tele.Video{File: file}
+				return c.Send(postcard, markup)
+			} else {
+				postcard := &tele.Photo{File: file}
+				return c.Send(postcard, markup)
+			}
+		}
+	case "Подключить рассылку📬":
+		{
+			return c.Send("Чтобы подключить рассылку напиши /time *time*, например /time 08:30")
+		}
+	case "Отключить рассылку📬":
+		{
+			//TODO delete user from db
+			return c.Send("Рассылка отключена😈")
 		}
 	}
+
 	return nil
 }
