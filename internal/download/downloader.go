c9cc433a3fee5a135d865c36c7aff2200d7eceed
ofdun <elik394@mail.ru>
2023-11-05 22:13:21 +0300

    Night postcard update added

--- internal/download/downloader.go
+++ internal/download/downloader.go
@@ -8,6 +8,8 @@ import (
 	"strconv"
 	"strings"
 	"telegramBot/internal/model"
+	"telegramBot/internal/parser"
+	"telegramBot/internal/storage"
 	"time"
 )
 
@@ -109,5 +111,57 @@ func PostcardDownload(path string, postcard *model.Postcard) error {
 		return err
 	}
 
+	return nil
+}
+
+func NightlyPostcardDownload() error {
+	postcardsDatabase := storage.NewDatabase()
+	postcardsStorage := storage.NewPostcardsPostgresStorage(postcardsDatabase)
+	if err := postcardsStorage.ClearDatabase(); err != nil {
+		return err
+	}
+
+	newParser := parser.NewParser(os.Getenv("POSTCARD_SITE"))
+
+	if err := newParser.GetHTML(); err != nil {
+		return err
+	}
+
+	holidays, err := newParser.GetHolidays()
+	if err != nil {
+		return err
+	}
+
+	postcards, err := newParser.GetPostcardsPages(holidays[0])
+
+	if err != nil {
+		return err
+	}
+
+	// Updating postcards ( adding links )
+	for i := range postcards {
+		if err = newParser.GetPostcardHref(&postcards[i]); err != nil {
+			return err
+		}
+	}
+	postcardsPath := os.Getenv("POSTCARD_PATH")
+	if err = os.RemoveAll(postcardsPath); err != nil {
+		return err
+	}
+	if err = os.Mkdir(postcardsPath, 0777); err != nil {
+		return err
+	}
+
+	for i := range postcards {
+		if err = PostcardDownload(postcardsPath, &postcards[i]); err != nil {
+			return err
+		}
+	}
+
+	for i := range postcards {
+		if err = postcardsStorage.AddPostcardToStorage(&postcards[i]); err != nil {
+			return err
+		}
+	}
 	return nil
 }
